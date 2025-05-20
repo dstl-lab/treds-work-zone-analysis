260866e871fd1d0440504a031f964b4d3f1fce92
Sam Lau <samlau95@gmail.com>
2025-05-20 16:36:21 -0700

    WIP

---
+++ docs/sparklines-v2/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Sparklines (v2)</h1>
+      <p>Hello world</p>
+    </>
+  );
+}
+
+export default App;
---
+++ docs/types.ts
@@ -0,0 +1,32 @@
+// workzones-metadata.json has a list of objects with the following schema:
+export interface WorkzoneMetadata {
+  workzone_id: string;
+  n: number;
+}
+
+// each workzone file has a list of objects with the following schema:
+export interface VehicleData {
+  vehicle_id: number;
+  district: number;
+  visit_date: Date; // Parsed from ISO date string
+  is_control_group: boolean;
+  workzone_id: string;
+  is_chins_reportable?: string;
+  cause?: string;
+  message?: string;
+  data: VehicleDataPoint[];
+}
+
+export interface VehicleDataPoint {
+  event_time: Date; // Parsed from string
+  speed: number;
+  acceleration?: number;
+}
+
+export interface ResampledData {
+  start_time: Date;
+  end_time: Date;
+  control_group_vehicles: VehicleData[]; // ordered by visit_date
+  test_group_vehicles: VehicleData[]; // ordered by visit_date
+  insufficient_control_data?: boolean; // Optional flag
+}
